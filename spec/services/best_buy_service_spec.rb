0364be82eb07842887d64e49c9419563214680b1
ToniRib <tonimarierib@gmail.com>
2016-03-08 10:55:11 -0700

    Finish testing BestBuyService

---
+++ spec/services/best_buy_service_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.describe BestBuyService, type: :model do
+  describe "#initialize" do
+    it "can be initialized with a search term" do
+      service = BestBuyService.new("sennheiser")
+
+      expect(service.search_term).to eq("sennheiser")
+      expect(service.connection.class).to eq(Faraday::Connection)
+      expect(service.response).to be_nil
+    end
+  end
+
+  describe "#find_products" do
+    it "returns an array of the first 15 matching products" do
+      service = BestBuyService.new("sennheiser")
+
+      products = VCR.use_cassette("search_sennheiser") do
+        service.find_products
+      end
+
+      first_product = products.first
+
+      expect(products.count).to eq(15)
+
+      expect(first_product.class).to eq(Product)
+      expect(first_product.respond_to?(:name)).to be true
+      expect(first_product.respond_to?(:short_description)).to be true
+      expect(first_product.respond_to?(:sale_price)).to be true
+      expect(first_product.respond_to?(:sku)).to be true
+      expect(first_product.respond_to?(:image)).to be true
+      expect(first_product.respond_to?(:customer_review_average)).to be true
+    end
+  end
+end
