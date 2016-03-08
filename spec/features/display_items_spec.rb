2a8a79cb41c536ebe5acbfc912e4f3d96317330e
ToniRib <tonimarierib@gmail.com>
2016-03-07 18:29:50 -0700

    Rewrite original integration test as a feature test in RSpec

---
+++ spec/features/display_items_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.feature "Display Items" do
+  scenario "it reaches the root path" do
+    visit '/'
+    expect(page.status_code).to eq(200)
+
+    within "h1" do
+      expect(page).to have_content('Items')
+    end
+  end
+end
