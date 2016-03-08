a5287e9928dcc16dcc9e0e282bd7389a18bd9e1a
ToniRib <tonimarierib@gmail.com>
2016-03-08 10:38:28 -0700

    update controller test for SearchController to use VCR

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -3,7 +3,9 @@ require "rails_helper"
 RSpec.describe SearchController, type: :controller do
   describe "GET #index" do
     it "returns a successful HTTP 200 status code" do
-      get :index
+      VCR.use_cassette("search_sennheiser") do
+        get :index, search: "sennheiser"
+      end
 
       expect(response.status).to eq(200)
       expect(response).to be_successful
