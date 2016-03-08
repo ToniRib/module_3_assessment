9182627a3d35273a2962735392345d35cf0df531
ToniRib <tonimarierib@gmail.com>
2016-03-08 11:25:30 -0700

    Add feature to Product class - if review is nil, set it to Not Available

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,11 +4,17 @@ class Product
 
   def initialize(name:, sku:, image:, short_description:,
                  sale_price:, customer_review_average:)
-    @name = name
-    @sku = sku
-    @image = image
-    @short_description = short_description
-    @sale_price = sale_price
-    @customer_review_average = customer_review_average
+    @name                    = name
+    @sku                     = sku
+    @image                   = image
+    @short_description       = short_description
+    @sale_price              = sale_price
+    @customer_review_average = customer_review_average || not_available
+  end
+
+  private
+
+  def not_available
+    "Not Available"
   end
 end
